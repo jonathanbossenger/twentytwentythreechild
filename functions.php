56f3af9adeea25f130f3f2a73e57d5c7aab7d799
Jonathan Bossenger <jonathanbossenger@gmail.com>
2023-05-04 14:41:48 +0200

    Multisite network name

--- functions.php
+++ functions.php
@@ -2,9 +2,11 @@
 
 if ( ! function_exists( 'tt3c_get_site_name' ) ) {
 	function tt3c_get_site_name() {
-		$blog_id   = get_current_blog_id();
 		$site_name = get_bloginfo( 'name' );
+		switch_to_blog( 1 );
+		$main_site_name = get_bloginfo( 'name' );
+		restore_current_blog();
 
-		return $site_name . ' - Site ID # ' . $blog_id;
+		return $site_name . ' (part of the ' . $main_site_name . ' network)';
 	}
 }
